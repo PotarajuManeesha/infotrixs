f9003d2456cca6752d1712d109f4f55d5558df89
PotarajuManeesha <141435306+PotarajuManeesha@users.noreply.github.com>
2023-08-08 17:42:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function startCourse(courseId) {
+    // You can replace this with actual logic to load course content
+    alert(`Starting Course ${courseId}`);
+    // You could redirect to a dedicated course page or load content dynamically
+  }
+  
